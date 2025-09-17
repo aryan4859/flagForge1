3e52cfef15379bf74220769a7ee303c26e36a40b
hadeyghoptey <hadamanash2023@gmail.com>
2025-09-17 12:59:57 +0545

    redirect to /profile on loggin in

--- app/(main)/authentication/page.tsx
+++ app/(main)/authentication/page.tsx
@@ -10,7 +10,7 @@ const AuthPage = () => {
 
   useEffect(() => {
     if (sessionStatus === "authenticated") {
-      router.replace("/home");
+      router.replace("/profile");
     }
   }, [sessionStatus, router]);
 
